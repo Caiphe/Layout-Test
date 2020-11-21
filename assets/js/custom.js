ec10cb028d4edb7266c9a887c5c1f235c0f86187
Marc-Ilunga Caiphe <caipheilunga@gmail.com>
2020-11-21 19:08:56 +0200

    layout dev

---
+++ assets/js/custom.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+
+AOS.init();
+
+$('.testimonials-block').slick({
+
+    slidesToShow: 3,
+    slidesToScroll: 3,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          infinite: false,
+          speed: 300,
+          arrows : false
+        }
+      }
+    ]
+  });
+})
+
+// Multiple Form Block
+var currentTab = 0;
+showTab(currentTab);
+
+function showTab(n) {
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+    document.getElementById("gray-arrow").style.display = "inline";
+
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+    document.getElementById("gray-arrow").style.display = "none";
+  }
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  var x = document.getElementsByClassName("tab");
+  if (n == 1 && !validateForm()) return false;
+  x[currentTab].style.display = "none";
+  currentTab = currentTab + n;
+  if (currentTab >= x.length) {
+    document.getElementById("regForm").submit();
+    return false;
+  }
+  showTab(currentTab);
+}
+
+function validateForm() {
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  for (i = 0; i < y.length; i++) {
+    if (y[i].value == "") {
+      y[i].className += " invalid";
+      valid = false;
+    }
+  }
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid;
+}
+
+function fixStepIndicator(n) {
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  x[n].className += " active";
+}
